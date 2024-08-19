a67e795c38c95cdc6deb29939d134bcb3cb1a97a
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-08-19 18:09:00 -0300

    js galeia index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-function proximo() {
+let index = 0; //variavel que vai ser a imagem que vai estar visivel na hora 
+const slides = document.querySelectorAll('.imgCarossel img'); //ele vais selecionar e armazenar todas as imagens com classe .imgCarossel
+const totalSlides = slides.length; // uma variavel pra armazenar a quantidade de imaagens que tem 
 
+function galeriaVisivel() {
+    document.querySelector('.imgCarossel').style.transform = 'translateX(' + (-index * 100) + '%)';
+    // querySelector seleciona o primeiro elemento encontrado, no caso das imagens que tem class imgCarossel
+    // o traslate x é pq ele ta momento um elemento no eixo x, ou seja para o lado 
+    // o - é pra mover pra esquerda
+    //o index x 100 é pq ele descola 100% para o lado 
+}
+
+function proximo() {
+    if (index < totalSlides -1) { // o -1 é pq a contagem de indicie começa no 0 ai se tirar ele vai 1 a mais
+        index++;
+        galeriaVisivel();
+    }
 }
 
 function anterior() {
-    
+    if (index > 0) {
+        index--;
+        galeriaVisivel();
+    }
 }
