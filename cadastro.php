aa6020dcc8f7db9271dff9c000a59929bc1e0e39
Gabriel-Britoo <gabrito343@gmail.com>
2024-11-14 14:10:35 -0300

    nav nas outras paginas

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,6 @@
 <?php
 include 'conexao.php';
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nome = $_POST["nome"];
@@ -41,17 +42,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="./css/login.css">
 </head>
 <body>
-    <header>
+<header>
         <div id="container">
-            <a href="index.php"><img class= "logo" src="./img/logo.png" alt="logo"></li></a>
+            <a href="index.php" id="box-img"><img class= "logo" src="./img/HC-logo.svg" alt="logo"></li></a>
             <nav>
                 <ul id="nav1">
-                    <li><h3><a href="./index.php">início</a></h3></li>
+                    <li><h3><a id="inicio" href="./index.php">início</a></h3></li>
                     <li><h3><a href="./servicos.php">Serviços</a></h3></li>
                     <li><h3><a href="./reservar.php">Reservar</a></h3></li>
                     <li><h3><a href="./contato.php">Contato</a></h3></li>
-                    <li><h3><a id="login" href="./login.php">Entrar</a></h3></li>
                 </ul>
+                <div id="user-div">
+                    <?php
+                    if (isset($_SESSION['nome']) && $_SESSION['nome'] != ''){
+                        echo "<select name='' id='user' onchange='sair()'>
+                                <option value='' id='opt-nome'>".$_SESSION['nome']."</option>
+                                <a><option value='' id='opt-sair'>Sair</option></a>
+                            </select>";
+                    } elseif (isset($_SESSION['nome']) && $_SESSION['nome'] == '') {
+                        echo "<h3><a id='login' href='./login.php'>Entrar</a></h3>";
+                    }
+                    ?>
+                    <script>
+                        function sair(){
+                            window.location.href = "./logout.php";
+                        }
+                    </script>
+                </div>
                 <input type="checkbox" id="checkbox">
                 <label for="checkbox" id="botao">☰</label>
                 <ul id="nav2">
@@ -59,13 +76,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <li><h3><a href="./servicos.php">Serviços</a></h3></li>
                     <li><h3><a href="./reservar.php">Reservar</a></h3></li>
                     <li><h3><a href="./contato.php">Contato</a></h3></li>
-                    <?php
-                    if ($_SESSION['nome'] != ''){
-                        echo "<li><h3><a href='./logout.php'>Sair</a></h3></li>";
-                    } elseif ($_SESSION['nome'] == '') {
-                        echo "<li><h3><a href='./login.php'>Entrar</a></h3></li>";
-                    }
-                    ?>
                 </ul>
             </nav>
         </div>
--- ocupacoes.php
+++ ocupacoes.php
@@ -15,40 +15,41 @@
     <title>Veja nossas ocupações</title>
 </head>
 <body>
-    <header>
+<header>
         <div id="container">
-            <a href="index.php"><img class= "logo" src="./img/logo.png" alt="logo"></li></a>
+            <a href="index.php" id="box-img"><img class= "logo" src="./img/HC-logo.svg" alt="logo"></li></a>
             <nav>
                 <ul id="nav1">
-                    <li><h3><a href="./index.php">início</a></h3></li>
+                    <li><h3><a id="inicio" href="./index.php">início</a></h3></li>
                     <li><h3><a href="./servicos.php">Serviços</a></h3></li>
                     <li><h3><a href="./reservar.php">Reservar</a></h3></li>
                     <li><h3><a href="./contato.php">Contato</a></h3></li>
+                </ul>
+                <div id="user-div">
                     <?php
                     session_start();
-
-                    if (isset($_SESSION['nome']) != ''){
-                        echo "<li><h3><a id='sair' href='./logout.php'>Sair</a></h3></li>";
-                    } elseif (isset($_SESSION['nome']) == '') {
-                        echo "<li><h3><a id='login' href='./login.php'>Entrar</a></h3></li>";
+                    if (isset($_SESSION['nome']) && $_SESSION['nome'] != ''){
+                        echo "<select name='' id='user' onchange='sair()'>
+                                <option value='' id='opt-nome'>".$_SESSION['nome']."</option>
+                                <a><option value='' id='opt-sair'>Sair</option></a>
+                            </select>";
+                    } elseif (isset($_SESSION['nome']) && $_SESSION['nome'] == '') {
+                        echo "<h3><a id='login' href='./login.php'>Entrar</a></h3>";
                     }
                     ?>
-                </ul>
+                    <script>
+                        function sair(){
+                            window.location.href = "./logout.php";
+                        }
+                    </script>
+                </div>
                 <input type="checkbox" id="checkbox">
                 <label for="checkbox" id="botao">☰</label>
                 <ul id="nav2">
                     <li><h3><a href="./index.php">início</a></h3></li>
                     <li><h3><a href="./servicos.php">Serviços</a></h3></li>
-                    <li><h3><a href="./ocupacoes.php">Reservar</a></h3></li>
+                    <li><h3><a href="./reservar.php">Reservar</a></h3></li>
                     <li><h3><a href="./contato.php">Contato</a></h3></li>
-
-                    <?php
-                    if ($_SESSION['nome'] != ''){
-                        echo "<li><h3><a href='./logout.php'>Sair</a></h3></li>";
-                    } elseif ($_SESSION['nome'] == '') {
-                        echo "<li><h3><a href='./login.php'>Entrar</a></h3></li>";
-                    }
-                    ?>
                 </ul>
             </nav>
         </div>
